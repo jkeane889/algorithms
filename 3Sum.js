d0581652396604d8f44759c5bbfd65bd5937b746
jkeane889 <jkeane889@gmail.com>
2020-06-24 10:52:53 -0500

    Initial attempt of 3Sums before time out exceeded error

--- 3Sum.js
+++ 3Sum.js
@@ -1,19 +1,18 @@
-// 3Sum
-
 /*
-    Given an array nums of n integers, are there elements a, b, c in nums 
-    such that a + b + c = 0? 
+    Given an array nums of n integers, are there elements a, b, c
+    in nums such that a + b + c = 0? 
     
     Find all unique triplets in the array which gives the sum of zero.
 
-    Note:  The solution set must not contain duplicate triplets.
+    Note:
+
+    The solution set must not contain duplicate triplets.
 
     Example:
 
     Given array nums = [-1, 0, 1, 2, -1, -4],
 
     A solution set is:
-
         [
             [-1, 0, 1],
             [-1, -1, 2]
@@ -22,73 +21,61 @@
 */
 
 /*
-    I - array of nums of n integers
-    O - all triplet, non-duplicate solution sets that equal zero
-    C - having to iterate multiple times over array sequence (m * n)
-    E - no triplet sequences that equal 0
-
+    I - array of unsorted integers
+    O - array of arrays where each sub array is a triplet sequence that sums to zero
+    C - time complexity (multiple traversals through array)
+    E - empty array; no sequences of 3 that equal zero; duplicate sequences
 */
 
 /*
-    Brute Force Approach
+    Create global storage array for triplet subsequences
 
-    Create object/hashmap to store all combinations
-    Create a current sum equal to elements from iteration added together
+    Brute-Force Approach
 
-    Three for loops that iterate over each item
+        Create sum = null
 
-        if currentSum === 0 && the current combination does not exist in hashmap && current combination's length === 3
-            add combination to hashmap
-            reset currentSum to empty array (use reduce to check currentSum)
-
+        Iterate over array with for loop
+            iterate over array with inner for loop (while i < i - 1)
+                iterate over array again with another inner for loop (while i < i - 2)
+                    create a new array
+                        if new array.length < 3
+                            push in element
+                        else if array.length === 3
+                            check if sum of element in array === 0
+                                if they do, add this array to sums array
+                            else
+                                remove last element from array
+                    
+        return array of arrays with triplet sequences
 */
 
-/*
-    Recursive/Backtracking Approach
+// Initial attempt
 
-    Create global hashmap to store sequences
-
-    base case: if current combination array's length === 3
-                 if sum of element's in combination === 0 && hashmap does not contain sequence
-                    add sequence to hashmap and return
-                 else
-                    return
-
-    recursive case:
-
-        iterate over each element in the array
-            add item to currentSumArray
-                slice array from i and recurse
-                add element back to array from having been sliced
-    
-    return hashmap.values
-
-*/
-
-const threeSum = nums => {
+const threeSum = (nums) => {
     if (!nums.length) {
-        return null
+        return []
     }
 
-    let hash = {}
-    let sequences = []
+    let sums = {}
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        for (let j = i + 1; j < nums.length - 1; j++) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
             for (let k = j + 1; k < nums.length; k++) {
-                const a = nums[i]
-                const b = nums[j]
-                const c = nums[k]
-                const set = [a, b, c]
-                let sortedSet = set.sort()
+                let newSum = []
+                newSum.push(nums[i], nums[j], nums[k])
+                let sum = newSum .reduce((acc, val) => {
+                    return acc + val
+                })
 
-                if (a + b + c === 0 && !hash[sortedSet]) {
-                    sequences.push(sortedSet)
-                    hash[sortedSet] = true
-                }
+                if (sum === 0) {
+                    newSum.sort()
+                    if (!sums[newSum]) {
+                        sums[newSum] = newSum
+                    }
+                }           
             }
         }
     }
 
-    return sequences
+    return Object.values(sums)
 };
